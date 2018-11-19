8e2ba3f8019ff5d147cd97d5b856b92586d7e66f
Trishan Valodia <trishan@dataedge.co.za>
2018-11-19 14:24:29 +0200

    Add missing PlayerControllerType class

---
+++ src/main/java/com/alltimeslucky/battletron/player/controller/PlayerControllerType.java
@@ -0,0 +1,9 @@
+package com.alltimeslucky.battletron.player.controller;
+
+public enum PlayerControllerType {
+    KEYBOARD,
+    KEYBOARD_WASD_KEYS,
+    KEYWORD_ARROW_KEYS,
+    AI_DOWNLEFT,
+    AI_SIMPLE
+}
