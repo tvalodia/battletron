32f5b2c7ba36e733f0bac21402c7c3379bff9c79
Trishan Valodia <trishan@dataedge.co.za>
2019-01-18 21:19:07 +0200

    Add missing Angular files to git.

---
+++ webapp/src/app/join-game/join-game.component.ts
@@ -0,0 +1,49 @@
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {GameService} from "../api/game.service";
+import {WebsocketService} from "../game-view/websocket.service";
+import {Game, GameViewService} from "../game-view/game-view.service";
+import {GameViewComponent} from "../game-view/game-view.component";
+
+@Component({
+  selector: 'app-join-game',
+  templateUrl: './join-game.component.html',
+  styleUrls: ['./join-game.component.css'],
+  providers: [WebsocketService, GameViewService]
+})
+export class JoinGameComponent implements OnInit {
+
+  playerId: string = '';
+  games: Array<Game>;
+  @ViewChild('gameView') gameViewRef: GameViewComponent;
+
+  constructor(private gameService: GameService) {
+  }
+
+  ngOnInit() {
+    this.getGames();
+  }
+
+  onNewPlayerId(playerId: string) {
+    this.playerId = playerId;
+  }
+
+  public getGames() {
+    this.gameService.getGames().subscribe((data: Array<Game>) => {
+      this.games = data;
+      console.log(data);
+    });
+  }
+
+  public refresh() {
+    this.getGames();
+  }
+
+  public joinGame(gameId: number) {
+    this.gameService.joinGame(gameId, this.playerId).subscribe((data: Game) => {
+      this.gameViewRef.setGame(data);
+      console.log(gameId);
+    });
+
+  }
+
+}
