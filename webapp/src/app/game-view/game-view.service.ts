158b850c9f35b310353852b0d6792535ec3f86c5
Trishan Valodia <trishan@dataedge.co.za>
2019-02-22 17:36:57 +0200

    Remove hardcoded URL
    - Remove the hardcoded URL from the GameView Angular service so that the Angular client can be run from any machine on the network.

--- webapp/src/app/game-view/game-view.service.ts
+++ webapp/src/app/game-view/game-view.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {Subject} from 'rxjs/Rx';
 import {WebsocketService} from './websocket.service';
 
-const GAME_URL = 'ws://localhost:8080/player';
+const GAME_URL = 'ws://' + document.location.host + '/player';
 
 export interface Game {
   id: number;
@@ -28,6 +28,8 @@ export class GameViewService {
   public subject: Subject<string>;
 
   constructor(public wsService: WebsocketService) {
+    console.log(document.location);
+
     this.subject = <Subject<string>>wsService
       .connect(GAME_URL)
       .map((response: MessageEvent): string => {
